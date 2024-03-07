33d85efdf2886a20cf1ca50ab6979a9208ac3232
Virtu <maksimsarsov777@gmail.com>
2024-03-07 23:55:42 +0300

    Добавление сенсоров

---
+++ sensor.py
@@ -0,0 +1,10 @@
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from sqlalchemy import engine
+from app import app, db, socketio
+from app.models import InterfaceTypes, Interfaces, SystemMetrics
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'session': db.session, 'InterfaceTypes': InterfaceTypes, 'Interfaces': Interfaces, 'SystemMetrics': SystemMetrics, 'engine': engine}
+
